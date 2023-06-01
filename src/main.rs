61e92626cba764878721df96e5204def7a68823e
setrar <setrar@valiha.com>
2023-06-01 16:09:48 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn setup(
     // light
     commands.spawn(PointLightBundle {
             point_light: PointLight {
-                intensity: 1500.0,
+                intensity: 3000.0,
                 shadows_enabled: true,
                 ..default()
             },
